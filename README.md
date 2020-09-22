97d7d2502a7447ad72d4653ea6bdc3548392dcd8
jchoi92k <57017670+jchoi92k@users.noreply.github.com>
2020-09-22 06:59:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react_webproject
+ react_proj
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://joon:rCrtTmWaJopcYO7H@jchoiwebapp.jf41v.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
